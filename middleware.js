11688ce84b86317e1238a7eb01563d5e00f3ccb7
MurtadhaMohammed <mumu9943@gmail.com>
2023-10-29 00:28:04 +0300

    update middleware file2

--- middleware.js
+++ middleware.js
@@ -4,23 +4,23 @@ export function middleware(req) {
   const response = NextResponse.next(req);
 
   response.headers.set("Access-Control-Allow-Origin", "*");
-  response.headers.set(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  response.headers.set(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
-  );
-  response.headers.set("Access-Control-Allow-Credentials", true);
-  response.headers.set("Access-Control-Max-Age", "86400");
+  // response.headers.set(
+  //   "Access-Control-Allow-Methods",
+  //   "GET, POST, PUT, DELETE, OPTIONS"
+  // );
+  // response.headers.set(
+  //   "Access-Control-Allow-Headers",
+  //   "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
+  // );
+  // response.headers.set("Access-Control-Allow-Credentials", true);
+  // response.headers.set("Access-Control-Max-Age", "86400");
 
   return response;
 }
 
-// export const config = {
-//   matcher: "/dashboard/:path*"
-// };
+export const config = {
+  matcher: "/dashboard/:path*"
+};
 
 // matcher: [
 //   // "/api/activity/:path*",
