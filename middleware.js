a2fdae3050a6ea8c24f43be9904f0c6fbf4fbba7
MurtadhaMohammed <mumu9943@gmail.com>
2023-10-29 00:35:10 +0300

    update middleware file3

--- middleware.js
+++ middleware.js
@@ -1,22 +1,24 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 
-export function middleware(req) {
-  const response = NextResponse.next(req);
+// export function middleware(req) {
+//   const response = NextResponse.next(req);
 
-  response.headers.set("Access-Control-Allow-Origin", "*");
-  // response.headers.set(
-  //   "Access-Control-Allow-Methods",
-  //   "GET, POST, PUT, DELETE, OPTIONS"
-  // );
-  // response.headers.set(
-  //   "Access-Control-Allow-Headers",
-  //   "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
-  // );
-  // response.headers.set("Access-Control-Allow-Credentials", true);
-  // response.headers.set("Access-Control-Max-Age", "86400");
+//   response.headers.set("Access-Control-Allow-Origin", "*");
+//   response.headers.set(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PUT, DELETE, OPTIONS"
+//   );
+//   response.headers.set(
+//     "Access-Control-Allow-Headers",
+//     "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
+//   );
+//   response.headers.set("Access-Control-Allow-Credentials", true);
+//   response.headers.set("Access-Control-Max-Age", "86400");
 
-  return response;
-}
+//   return response;
+// }
+
+export { default } from "next-auth/middleware";
 
 export const config = {
   matcher: "/dashboard/:path*"
